dd247630a437cc6b2dea4cd7062c3d3b650d03ec
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-10 19:24:52 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,11 +7,12 @@ root.config(bg="black")
 root.title("Digital Clock")
 
 
-design = Frame(root, bg="black",highlightbackground="red",
-               highlightthickness=2)
-design.place(relx=0.5, rely=0.5, anchor="center",
-             height=350, width=350)
+design = Frame(root, bg="black",highlightbackground="red",highlightthickness=2)
+design.place(relx=0.5, rely=0.5, anchor="center",height=350, width=350)
+
+
 def clock():
+  
     now = datetime.datetime.now()
     current_time = now.strftime("%I:%M:%S %p")
     current_date = now.strftime("%A\n%d %B %Y")
@@ -25,4 +26,5 @@ label.place(relx=0.5, rely=0.5, anchor="center")
 
 clock()
 root.mainloop()
+
 
