7aaed30d70a7ed9aca2cac2eb260e5e0f824754e
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-11 19:17:48 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,19 @@ root.geometry("400x400")
 root.config(bg="black")
 root.title("Digital Clock")
 
+
+
+
+
+
+
 design = Frame(root, bg="black",highlightbackground="red",highlightthickness=2)
+
+
+
+
+
+
 design.place(relx=0.5, rely=0.5, anchor="center",height=350, width=350)
 
 
@@ -23,6 +35,7 @@ label.place(relx=0.5, rely=0.5, anchor="center")
 
 clock()
 root.mainloop()
+
 
 
 
