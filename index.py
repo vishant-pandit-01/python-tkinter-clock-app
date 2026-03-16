fda675c153fc7df19c532734f95e1ce44891acc6
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-16 20:37:03 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,10 +7,13 @@ root.geometry("400x400")
 root.config(bg="black")
 root.title("Digital Clock")
 
+
 design = Frame(root, bg="black",highlightbackground="red",highlightthickness=2)
 design.place(relx=0.5, rely=0.5, anchor="center",height=350, width=350)
 
+
 def clock():
+    
     now = datetime.datetime.now()
     current_time = now.strftime("%I:%M:%S %p")
     current_date = now.strftime("%A\n%d %B %Y")
