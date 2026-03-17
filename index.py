24a39cf03eafb3ff640ee8bb6577779a2940b8c7
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-17 20:02:25 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,13 @@
 from tkinter import *
 import datetime
-
 root = Tk()
 
 root.geometry("400x400")
 root.config(bg="black")
 root.title("Digital Clock")
 
-
 design = Frame(root, bg="black",highlightbackground="red",highlightthickness=2)
 design.place(relx=0.5, rely=0.5, anchor="center",height=350, width=350)
-
 
 def clock():
     
